3c0765af1f2220e1b450a5b079ac3f0e9ecb5e80
Stephanie B Burr <sbeusse@gmail.com>
2018-04-23 19:45:39 -0400

    set up db for heroku

--- config/connection.js
+++ config/connection.js
@@ -5,6 +5,16 @@ const pgp = require('pg-promise')();
 const config = require('./dbConfig');
 
 //connect to database
-const db = pgp(config)
+// const db = pgp(config)
+
+function setDatabase() {
+    if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
+        return pgp(config);
+    } else if (process.env.NODE_ENV === 'production') {
+        return pgp(process.env.DATABASE_URL);
+    }
+}
+
+const db = setDatabase();
 
 module.exports = db;
